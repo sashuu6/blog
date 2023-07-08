aa0e3c9fb889040c9169904526f0c486a7ee5a63
Sashwat K <sashwat0001@gmail.com>
2023-07-08 08:37:25 +0530

    feat: #10: add support for wsgi for production

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
  * Purpose : Host portfolio website using Python Flask
 """
 import json
-import os
 
 from flask import Flask, render_template
 
@@ -22,8 +21,3 @@ app = Flask(__name__, template_folder="template")
 def index() -> str:
     """Definition to render root HTML page"""
     return render_template('index.html', **DICTDATA)
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(threaded=True, host='0.0.0.0', port=port)
